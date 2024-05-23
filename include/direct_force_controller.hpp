aac36afdf4224dbc338a104c69a05c27e0f9e16e
rjros <81947104+rjros@users.noreply.github.com>
2024-05-24 00:44:12 +0900

    Include direct force controller

---
+++ include/direct_force_controller.hpp
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2024, Ricardo Rosales Martinez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file admittance_controller.hpp
+ * @brief Header file for the admittance force controller node, used for aerial interactions 
+ *
+ * @author Ricardo Rosales Martinez
+ */
+
+#ifndef DIRECT_FORCE_CONTROLLER_
+#define DIRECT_FORCE_CONTROLLER_
+
+
+// ROS2 libraries 
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/wrench_stamped.hpp> //leptrino wrench  f and torque*
+#include <geometry_msgs/msg/pose_stamped.hpp>
+
+#include <px4_msgs/msg/trajectory_setpoint.hpp>
+#include <interaction_msgs/msg/contact_setpoint.hpp>
+
+#include <Eigen/Dense> // msg type to be published with correction
+using std::placeholders::_1; // Place holder for ros subscriptions
+
+
+class DirectForceController : public rclcpp::Node {
+    
+    public: 
+        //// CONSTRUCTOR ////
+        /*constructor of the class*/
+        DirectForceController();
+    
+        //Destructor//
+        ~DirectForceController();
+    private:
+        
+        void initNode();
+        // void declareParameters();
+        void initAdmittance();
+        void computeAdmittance();
+
+        //ROS Related callbacks
+        void TrajectorySubCallback(const interaction_msgs::msg::ContactSetpoint::SharedPtr msg);
+        void WrenchSubCallback(const geometry_msgs::msg::WrenchStamped::SharedPtr msg);
+        void publishMessage();
+        
+        //// ROS variables ////
+        rclcpp::Subscription<interaction_msgs::msg::ContactSetpoint>::SharedPtr trajectory_sub_; // Later change to PX4 msg sub type
+        rclcpp::Subscription<geometry_msgs::msg::WrenchStamped>::SharedPtr wrench_sub_; //PX4 msg sub type
+        rclcpp::Publisher<px4_msgs::msg::TrajectorySetpoint>::SharedPtr publisher_;
+        rclcpp::TimerBase::SharedPtr timer_;
+
+        //Internal variables
+    
+        std::vector<double> trajectoryRef_ = std::vector<double>(4,0);
+        std::vector<double> positionSp_ = std::vector<double>(3,0);
+        float yawRf_;
+        float yawSp_; 
+
+
+        std::vector<double>quaternionRef_ = std::vector<double>(4,0);
+        std::vector<double>quaternionSp_ = std::vector<double>(4,0);
+
+        std::vector<double> forceRef_ = std::vector<double>(3,0);
+        std::vector<double>torqueRef_ = std::vector<double>(3,0);
+        std::vector<double> eulerRef_ = std::vector<double>(3,0);
+
+        // Inertial (M), Damping (D) and Stiffness (K) matrices
+
+        double K_x{0};
+        double K_z{0};
+        double K_yaw{0};
+
+
+
+        int rate_;
+        // Force Control internal varaibles 
+        double forceSp_;
+        bool contactRef_{false};
+
+
+};
+
+#endif 
---
+++ src/direct_force_controller.cpp
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2024, Ricardo Rosales Martinez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file admittance_controller.cpp
+ * @brief Admittance force controller node, used for aerial interactions 
+ *
+ * @author Ricardo Rosales Martinez
+ */
+
+
+#include "direct_force_controller.hpp"
+#include <cstdio>
+#include <algorithm>
+#include <iterator>
+
+
+DirectForceController :: DirectForceController() : Node("admittance_wrench_control") 
+{
+  
+  //initialization ROS node
+  RCLCPP_INFO(this->get_logger(), "%s\n","I am initializing the ROS node...");
+  initNode();
+  initAdmittance();
+
+}
+
+
+void DirectForceController :: initNode()
+{
+  int ms_time=10;//1000/rate_;
+  trajectory_sub_= this->create_subscription<interaction_msgs::msg::ContactSetpoint>("/raw/trajectory_setpoint", 10, 
+  std::bind(&DirectForceController::TrajectorySubCallback, this,_1));
+  wrench_sub_= this->create_subscription<geometry_msgs::msg::WrenchStamped>("leptrino_force_sensor/sensor_wrench", 1, 
+  std::bind(&DirectForceController::WrenchSubCallback, this,_1));
+  publisher_= this->create_publisher<px4_msgs::msg::TrajectorySetpoint>("/fmu/in/trajectory_setpoint",10);
+  timer_ = this->create_wall_timer(std::chrono::milliseconds(ms_time), std::bind(&DirectForceController::publishMessage, this));
+  
+}
+
+//// ROS Functions ////
+
+void DirectForceController :: publishMessage()
+{
+    computeAdmittance();
+
+    auto message = std::make_unique<px4_msgs::msg::TrajectorySetpoint>();
+    message->timestamp = 0; // Set timestamp to microseconds
+    
+    // Final position should be the XD*=XD + f/k
+    //pushing only to the front X axis of the UAV, convert to Body Frame
+    // message->pose.position.x = std::round(positionSp_[0]*1000)/1000;   // Set position (example values)
+    // message->pose.position.y = positionSp_[1];
+    // message->pose.position.z = positionSp_[2];
+
+    message->position[0]=positionSp_[0];
+    message->position[1]=positionSp_[1];
+    message->position[2]=positionSp_[2];
+    message->yaw=yawSp_;
+
+    RCLCPP_INFO(this->get_logger(), "x pos %f y pos %f z pos %f yaw %f\n" ,positionSp_[0],positionSp_[1],positionSp_[2],yawSp_);
+    
+    publisher_->publish(std::move(message));
+
+
+}
+
+void DirectForceController :: TrajectorySubCallback(const interaction_msgs::msg::ContactSetpoint::SharedPtr msg )
+{
+  auto position =msg->position;
+  auto yaw = msg->yaw;
+  contactRef_=msg->contact;
+  yawRf_ = yaw;
+  trajectoryRef_={position[0],position[1],position[2],yawSp_};
+
+
+  // RCLCPP_INFO(this->get_logger(), "Force X %f Force Y %f Force Z %f", forceRef_[0], forceRef_[1], forceRef_[2]);
+
+  // std::copy(std::begin(position),std::end(position),std::begin(refPosition_));
+
+}
+
+void DirectForceController :: WrenchSubCallback(const geometry_msgs::msg::WrenchStamped::SharedPtr msg )
+{
+  auto force= msg->wrench.force;
+  auto torque= msg->wrench.torque;
+
+  // Assign force and torque vectors directly to refForce_ and refTorque_
+  forceRef_ = {force.x, force.y, force.z};
+  torqueRef_ = {torque.x, torque.y, torque.z};
+}
+
+//// ROS Functions END ////
+
+
+void DirectForceController::initAdmittance()
+{
+  // Get parameters from config file regarding the M, D and K matrices
+  K_x=15;
+  K_z=1.2;
+  K_yaw=0.09;  
+  forceSp_=-3;
+
+  RCLCPP_INFO(this->get_logger(), "%s\n","Initializing admittance controller...");// string followed by a newline
+
+}
+void DirectForceController::computeAdmittance()
+{
+  // RCLCPP_INFO(this->get_logger(),"%s\n","Computing admittance controller...");
+  RCLCPP_INFO(this->get_logger(), "Force X %f Force Y %f Force Z %f", forceRef_[0], forceRef_[1], forceRef_[2]);
+
+  //// Force in the axis of the manipulator ////
+  /// Rotate wrt to the Yaw (B)
+  if(!(forceRef_[2]<-1.000))
+  {
+    positionSp_[0] = trajectoryRef_[0] ;//+ (forceRef_[2]-forceSp_)/K_;
+    positionSp_[1] = trajectoryRef_[1] ;//positionRef_[1]; 
+    positionSp_[2] = trajectoryRef_[2] ;//positionRef_[2];
+    yawSp_=0;
+  }
+
+  else 
+  {
+    positionSp_[0] = trajectoryRef_[0] - (forceSp_-forceRef_[2])/K_x; // current positon 
+    positionSp_[1] = trajectoryRef_[1];// + (forceRef_[1])/100;//positionRef_[1]; 
+    positionSp_[2] = trajectoryRef_[2]+ (forceRef_[0])/K_z;// + (forceRef_[2]-0)/K_;//positionRef_[2];
+    yawSp_=((trajectoryRef_[3]-forceRef_[1]/K_yaw))*3.14/180;
+
+  }
+
+
+}
+
+DirectForceController :: ~DirectForceController()
+{
+ RCLCPP_INFO(this->get_logger(), "Shutting down node.");
+}
+
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc,argv);
+  std::shared_ptr<DirectForceController> node = std::make_shared<DirectForceController>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+
+}
